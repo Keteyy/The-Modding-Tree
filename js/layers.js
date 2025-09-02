a355df884beb695fa9cf664aece8a4e551ce4b7c
Keteyy <225772294+Keteyy@users.noreply.github.com>
2025-09-02 16:16:12 -0700

    Second layer
    second layer

--- js/layers.js
+++ js/layers.js
@@ -58,3 +58,65 @@ addLayer("p", {
         return mult
     },
 })
+
+addLayer("d", {
+    name: "Dragons", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() {
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
+    color: "#7e8a79ff",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Dragons", // Name of prestige currency
+    baseResource: "Scales", // Name of resource prestige is based on
+    baseAmount() { return player.points }, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        { key: "d", description: "P: Reset for prestige points", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+    ],
+    layerShown() { return true },
+    upgrades: {
+        11: {
+            title: "Make this whatever you want!",
+            description: "Double your point gain.",
+            cost: new Decimal(1),
+
+        },
+        12: {
+            title: "Make this whatever you want!",
+            description: "Multiply",
+            cost: new Decimal(1),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id)) + "x" }, // Add formatting to the effect
+        },
+        13: {
+            title: "Upgrade 3",
+            description: "Double your point gain.",
+            cost: new Decimal(1),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
+        },
+    },
+        gainMult() {
+        let mult = new Decimal(1)
+        if (hasUpgrade('d', 13)) mult = mult.times(upgradeEffect('d', 13))
+        return mult
+    },
+})
+
